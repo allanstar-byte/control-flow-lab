6450ca170b5117616f2e35696c165e8cb56ffd4b
allanstar-byte <allanmutisya263@gmail.com>
2022-06-07 05:03:37 +0300

    Control Flows

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const { Console } = require("console");
+
+function scuberGreetingForFeet(ride) {
+    if (ride < 400 && ride < 2500) {
+        return 'This one is on me!'
+    } else if (ride > 2000) {
+        return 'I will gladly take your thirty bucks.'
+    }
+}
+scuberGreetingForFeet(2000)
+
+
+function ternaryCheckCity(city) {
+    if (city === 'NYC') {
+        return 'Ok, sounds good.'
+    } else {
+        return 'No go.'
+    }
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity(NYC)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(y) {
+    // Write your code here!
+    switch (y) {
+        case 'generous':
+            return 'Thank you so much.'
+        case 'not as generous':
+            return 'Thank you.'
+        default:
+            return 'Bye.'
+
+    }
 }
